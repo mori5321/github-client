17b7b889257f9ccb5cf8371821c8dbe0d694722c
mori5321 <moriwm77@gmail.com>
2019-10-28 19:23:57 +0900

    get issue function

--- src/GitHub/Requests/Issues/List.hs
+++ src/GitHub/Requests/Issues/List.hs
@@ -2,8 +2,14 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module GitHub.Requests.Issues.List
-  ( listUserIssues
+  ( listIssues
+  , listIssuesHttpRequest
+  , listUserIssues
   , listUserIssuesHttpRequest
+  , listOrganizationIssues
+  , listOrganizationIssuesHttpRequest
+  , getIssue
+  , getIssueHttpRequest
   )
 where
 
@@ -26,8 +32,26 @@ import GitHub.Request ( Request(..)
                       , withQuery
                       )
 import GitHub.Auth ( Auth )
+import GitHub.Error (Error, parseBodyEither, getResponseBodyEither)
 
-    
+listIssuesHttpRequest :: [QueryItem] -> Auth -> HTTP.Request
+listIssuesHttpRequest queryItems auth = withAuth auth
+                                        . withQuery query
+                                        . mkHttpRequest $ req
+  where
+    req = Request { reqPath = mconcat ["/issues"]
+                  , reqMethod = GET }
+    query = map toHTTPQueryItem queryItems
+
+                             
+listIssues :: [QueryItem] -> Auth -> IO (Either Error [Issue])
+listIssues queryItems auth =
+    getResponseBodyEither <$> sendRequest' httpReq
+   where
+    httpReq = listIssuesHttpRequest queryItems auth
+
+
+
 listUserIssuesHttpRequest :: [QueryItem] -> Auth -> HTTP.Request
 listUserIssuesHttpRequest queryItems auth = withAuth auth
                                            . withQuery query
@@ -37,12 +61,63 @@ listUserIssuesHttpRequest queryItems auth = withAuth auth
                   , reqMethod = GET }
     query = map toHTTPQueryItem queryItems
 
-
-listUserIssues :: [QueryItem] -> Auth -> IO (Either Int [Issue])
-listUserIssues queryItems auth = do
-    res <- getResponseBody <$> sendRequest' httpReq
-    case res of
-      Right a -> return $ Right a
-      Left (HTTP.JSONConversionException a b c) -> return $ Left $ HTTP.getResponseStatusCode b
+-- # Usage
+-- Right res <- listUserIssues [QueryItem Created] auth
+-- mapM_ Data.Text.IO.putStrLn $ map GitHub.Types.Issue.title res
+listUserIssues :: [QueryItem] -> Auth -> IO (Either Error [Issue])
+listUserIssues queryItems auth =
+    getResponseBodyEither <$> sendRequest' httpReq
   where
     httpReq = listUserIssuesHttpRequest queryItems auth
+
+
+
+type OrganizationName = S8.ByteString
+
+listOrganizationIssuesHttpRequest :: OrganizationName ->
+                                     [QueryItem] ->
+                                     Auth ->
+                                     HTTP.Request
+listOrganizationIssuesHttpRequest name queryItems auth =
+    withAuth auth . withQuery query . mkHttpRequest $ req
+  where
+    req = Request { reqPath = S8.intercalate "/" ["orgs", name, "issues"]
+                  , reqMethod = GET }
+    query = map toHTTPQueryItem queryItems
+
+
+listOrganizationIssues :: OrganizationName ->
+                          [QueryItem] ->
+                          Auth ->
+                          IO (Either Error [Issue])
+listOrganizationIssues name queryItems auth =
+    getResponseBodyEither <$> sendRequest' httpReq
+  where
+    httpReq = listUserIssuesHttpRequest queryItems auth
+
+
+type OwnerName = S8.ByteString
+type RepoName = S8.ByteString
+type IssueNumber = Int
+
+getIssueHttpRequest :: OwnerName ->
+                       RepoName ->
+                       IssueNumber ->
+                       Auth ->
+                       HTTP.Request
+getIssueHttpRequest ownerName repoName issueNumber auth =
+    withAuth auth . mkHttpRequest $ req
+  where
+    req = Request { reqPath = S8.intercalate "/" ["repos", ownerName, repoName, "issues", S8.pack . show $ issueNumber]
+                  , reqMethod = GET
+                  }
+
+getIssue :: OwnerName ->
+            RepoName ->
+            IssueNumber ->
+            Auth ->
+            IO (Either Error Issue)
+getIssue ownerName repoName issueNumber auth =
+    getResponseBodyEither <$> sendRequest' httpReq
+  where
+    httpReq = getIssueHttpRequest ownerName repoName issueNumber auth
